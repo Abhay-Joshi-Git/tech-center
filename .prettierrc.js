b505a175a5347e6f1edac1d7850c3260f7a147fa
Awadhoot <>
2019-11-11 09:58:29 +0530

    initial commit - added boilerplate

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "semi": true,
+  // "overrides": [
+  //   {
+  //     "files": "*.test.js",
+  //     "options": {
+  //       "semi": true
+  //     }
+  //   },
+  //   {
+  //     "files": ["*.html", "legacy/**/*.js"],
+  //     "options": {
+  //       "tabWidth": 4
+  //     }
+  //   }
+  // ]
+  "printWidth": 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100
+}
